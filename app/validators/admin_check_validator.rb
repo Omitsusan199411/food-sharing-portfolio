20c50aa81850c176c2b81cffa841c2cd1e613fb3
Omitsusan199411 <omitsusan5014@gmail.com>
2021-08-08 01:25:07 +0900

    条件分岐の変更、shopモデルバリデーション条件変更

--- app/validators/admin_check_validator.rb
+++ app/validators/admin_check_validator.rb
@@ -2,7 +2,7 @@ class AdminCheckValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     unless value =~ /\A[ぁ-んァ-ン一-龥]+　{0}[ぁ-んァ-ン一-龥]+\z/
-      record.errors[attribute] << "名の半角スペースは姓と名の間に入れてください"
+      record.errors[attribute] << "名の全角スペースは姓と名の間に入れてください"
     end
   end
   
