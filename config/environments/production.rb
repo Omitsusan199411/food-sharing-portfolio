172e22a368790ab53f54ea47caf0c2e3ff1d91ec
Omitsusan199411 <omitsusan5014@gmail.com>
2021-12-08 03:24:19 +0900

    Nginx設定ファイルのドキュメントルートパスの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # trueの場合は、public/assetsの中に必要なファイルが見つからなかった時にapp/assetsなどからファイルから探しに行くコンパイル処理を行う。
-  # app/assetsに静的ファイルを保存している場合は、trueにしないとcssやjsがブラウザに反映されない。
+  # app/assetsに静的ファイルを保存している場合は、trueにしないとcssやjsがブラウザに反映されない。developmentはデフォルトでtrueとなっている。
   config.assets.compile = true
   # プリコンパイル後にコンパイルされた静的ファイルにアルファベットの記号を付与する。ファイル名の末尾にハッシュ値が付く。
   # コードの内容からハッシュ値を算出しているので、コードが変更されればファイル名が変更されるためブラウザのキャッシュの影響で修正が反映されないなどの問題を防ぐ
--- config/prd_unicorn.rb
+++ config/prd_unicorn.rb
@@ -8,15 +8,19 @@ app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 2
 
+# unicornの起動コマンド（bundle exec "unicorn_rails -c config/prd_unicorn.rb -E prd"）が実行されるディレクトリ
 working_directory app_path
 
+# Nginxからのリクエストを受信するファイル（Nginxのunixドメインソケットのパスと同一のものにすること）
 listen '/var/www/app/tmp/sockets/unicorn.sock'
+# unicornのプロセスIDを保存するファイルを指定
 pid '/var/www/app/tmp/pids/unicorn.pid'
 
 # unicornのエラログー・出力ログを標準エラー・標準出力に吐き出す。標準出力→CloudWatchへ
 stderr_path = $stderr
 stdout_path = $stdout
 
+# ワーカープロセスが処理を開始し終了するまでの最大時間。この設定時間を超えるとワーカーは破棄される。
 timeout 60
 
 
@@ -27,14 +31,16 @@ check_client_connection false
 
 run_once = true
 
+# フォーク前の処理
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
+    # nicornとActiveRecord（データベース）との接続を切断する。
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
     run_once = false # prevent from firing again
   end
-
+  # マスタープロセスの再起動（古いunicornのpidをkill処理し、新しいpidを生成）
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exist?(old_pid) && server.pid != old_pid
     begin
@@ -46,6 +52,8 @@ before_fork do |server, worker|
   end
 end
 
+# フォーク後の処理
 after_fork do |_server, _worker|
+  # unicornとActiveRecord（データベース）との再接続を行う。
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
